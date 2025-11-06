687647b0d98475ba57a4c25b97aa5333e3af8755
aloshvp <92634532+aloshvp@users.noreply.github.com>
2025-11-06 21:37:03 +0530

    banner img

--- app/components/home/Banner.jsx
+++ app/components/home/Banner.jsx
@@ -1,44 +1,116 @@
-import React from 'react'
+// import React from 'react'
+
+// const Banner = () => {
+//     return (
+//         <section className="bannerWrap">
+//             <div className="container">
+//                 <div className="bannerContent">
+//                     <div className="bannerContentLft">
+//                         <h1>Our <em></em>
+//                             Business <em></em>
+//                             Lines</h1>
+//                         <p>We are one of the Leading Manufacturer and Technical <em></em>
+//                             Solution Provider in the field of Electrical Earthing and <em></em>
+//                             Lightning Protection. </p>
+//                     </div>
+//                     <div className="bannerContentRght">
+//                         <div className="bannerGrid">
+//                             <div className="bannerGridItem">
+//                                 <em>01</em>
+//                                 <h2>Electrical <em></em>
+//                                     Earthing</h2>
+//                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
+//                             </div>
+//                             <div className="bannerGridItem">
+//                                 <em>02</em>
+//                                 <h2>Lightning
+//                                     <em></em>
+//                                     Protection
+//                                 </h2>
+//                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
+//                             </div>
+//                             <div className="bannerGridItem">
+//                                 <em>03</em>
+//                                 <h2>Electrical <em></em>
+//                                     Enclosures</h2>
+//                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
+//                             </div>
+//                             <div className="bannerGridItem">
+//                                 <em>04</em>
+//                                 <h2>Cable <em></em>
+//                                     Management</h2>
+//                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default Banner
+
+"use client";
+import React from "react";
+import Image from "next/image";
 
 const Banner = () => {
     return (
         <section className="bannerWrap">
+            {/* Background image using Next.js Image */}
+            <div className="bannerBg">
+                <Image
+                    src="/images/home/banner-home-bg.jpg"
+                    alt="Banner Background"
+                    fill
+                    priority
+                    style={{ objectFit: "cover" }}
+                />
+                <div className="bannerOverlay"></div>
+            </div>
+
             <div className="container">
                 <div className="bannerContent">
                     <div className="bannerContentLft">
-                        <h1>Our <em></em>
-                            Business <em></em>
-                            Lines</h1>
-                        <p>We are one of the Leading Manufacturer and Technical <em></em>
-                            Solution Provider in the field of Electrical Earthing and <em></em>
-                            Lightning Protection. </p>
+                        <h1>
+                            Our <em></em> Business <em></em> Lines
+                        </h1>
+                        <p>
+                            We are one of the Leading Manufacturer and Technical <em></em>{" "}
+                            Solution Provider in the field of Electrical Earthing and{" "}
+                            <em></em> Lightning Protection.
+                        </p>
                     </div>
+
                     <div className="bannerContentRght">
                         <div className="bannerGrid">
                             <div className="bannerGridItem">
                                 <em>01</em>
-                                <h2>Electrical <em></em>
-                                    Earthing</h2>
+                                <h2>
+                                    Electrical <em></em> Earthing
+                                </h2>
                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
                             </div>
                             <div className="bannerGridItem">
                                 <em>02</em>
-                                <h2>Lightning
-                                    <em></em>
-                                    Protection
+                                <h2>
+                                    Lightning <em></em> Protection
                                 </h2>
                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
                             </div>
                             <div className="bannerGridItem">
                                 <em>03</em>
-                                <h2>Electrical <em></em>
-                                    Enclosures</h2>
+                                <h2>
+                                    Electrical <em></em> Enclosures
+                                </h2>
                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
                             </div>
                             <div className="bannerGridItem">
                                 <em>04</em>
-                                <h2>Cable <em></em>
-                                    Management</h2>
+                                <h2>
+                                    Cable <em></em> Management
+                                </h2>
                                 <p>Lorem Ipsum has been the industry's standard dummy text ever since</p>
                             </div>
                         </div>
@@ -46,7 +118,7 @@ const Banner = () => {
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Banner
+export default Banner;
