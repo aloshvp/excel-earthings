652cabaec3a5dde93c23359824fba8a4852df619
Vineesh <vineeshkm1998@gmail.com>
2025-11-11 22:48:23 +0530

    style updates

--- app/products/category/page.js
+++ app/products/category/page.js
@@ -1,9 +1,54 @@
+"use client"
 import "@styles/staticbundles.scss";
 import Link from 'next/link'
 import Image from 'next/image'
 import CommonBanner from "@common/CommonBanner";
-
+import { useState } from "react";
 const page = () => {
+    const[active,setActive]=useState("Electrical Earthing");
+
+    const productMenus = [
+    {
+        label: "Electrical Earthing",
+        subMenu: [
+            { label: "All", link: "" },
+            { label: "Earth Electodes", link: "" },
+            { label: "Earth Enhancing Compounds", link: "" },
+            { label: "Terminal Strip / Earth Bench", link: "" },
+            { label: "Earth Bit Chamber", link: "" }
+        ]
+    },
+    {
+        label: "Lightning Protection",
+        subMenu: [
+            { label: "All", link: "" },
+            { label: "Earth Electodes", link: "" },
+            { label: "Earth Enhancing Compounds", link: "" },
+            { label: "Terminal Strip / Earth Bench", link: "" },
+            { label: "Earth Bit Chamber", link: "" }
+        ]
+    },
+    {
+        label: "Electrical Enclosures",
+        subMenu: [
+            { label: "All", link: "" },
+            { label: "Earth Electodes", link: "" },
+            { label: "Earth Enhancing Compounds", link: "" },
+            { label: "Terminal Strip / Earth Bench", link: "" },
+            { label: "Earth Bit Chamber", link: "" }
+        ]
+    },
+    {
+        label: "Cable Management",
+        subMenu: [
+            { label: "All", link: "" },
+            { label: "Earth Electodes", link: "" },
+            { label: "Earth Enhancing Compounds", link: "" },
+            { label: "Terminal Strip / Earth Bench", link: "" },
+            { label: "Earth Bit Chamber", link: "" }
+        ]
+    },
+  ]
   return (
     <>
     <CommonBanner pageName={"category"}/>
@@ -11,17 +56,18 @@ const page = () => {
         <div className='container'>
             <div className='productSections'>
                 <div className='productLeftMenuSec'>
-                    <div className='productLeftMenu'>
-                        <span>Electrical Earthing</span>
-                        <ul>
-                            <li><Link href={""}>All</Link></li>
-                            <li><Link href={""}>Earth Electodes</Link></li>
-                            <li><Link href={""}>Earth Enhancing Compounds</Link></li>
-                            <li><Link href={""}>Terminal Strip / Earth Bench</Link></li>
-                            <li><Link href={""}>Earth Bit Chamber</Link></li>
-                        </ul>
-                    </div>
-                    <div className='productLeftMenu'>
+                   
+                    {productMenus.map((item,index)=>(
+                        <div className='productLeftMenu' key={index*2}>
+                            <span className={(active===item.label)?"active":""} onClick={()=>setActive(item.label)}>{item.label}</span>
+                            <ul className={(active===item.label)?"active":""}>
+                                {item?.subMenu?.map((item,index)=>(
+                                     <li key={index}><Link href={""} scroll={false}>{item.label}</Link></li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                    {/* <div className='productLeftMenu'>
                         <span>Lightning Protection</span>
                         <ul>
                             <li><Link href={""}>All</Link></li>
@@ -50,7 +96,7 @@ const page = () => {
                             <li><Link href={""}>Terminal Strip / Earth Bench</Link></li>
                             <li><Link href={""}>Earth Bit Chamber</Link></li>
                         </ul>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='productList'>
                     <div className='prdtBox'>
