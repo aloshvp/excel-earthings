d8f2b70077bfe67ad8806d452ad2d0c8b51ccb81
Vineesh <vineeshkm1998@gmail.com>
2025-11-12 20:58:56 +0530

    contact us images

---
+++ app/contact-us/page.js
@@ -0,0 +1,13 @@
+import "@styles/staticbundles.scss";
+
+const page = () => {
+  return (
+    <div className="contactUsWrapper">
+      <div className="container">
+          <h1>Contact Us</h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
