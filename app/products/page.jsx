c9f2c553930e7cf7c597713dd2743fec19d8d0b0
aloshvp <aloshvp12@gmail.com>
2025-11-14 00:09:48 +0530

    careers

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -4,24 +4,32 @@ import Link from "next/link";
 import { productsData } from "@utils/homeData";
 
 const page = () => {
+
+    const totalProducts = productsData?.length;
+
     return (
         <section className='productsMainWrap'>
-            {productsData?.map((product) =>
-                <div className="productsMainItem" key={product.number}>
-                    <div className="productsMainItemBg">
-                        <Image src={product.bgImage} alt={product.name} fill className="productsMainItemImg" priority />
-                    </div>
-                    <div className="productsMainItemContent">
-                        <span className="productsMainItemNumber">{product.number}</span>
-                        <h3 className="productsMainItemName">{product.name}</h3>
-                        <Link href={product?.link?product?.link:""} scroll={false}>
-                            Explore More <span className="arrow"></span>
-                        </Link>
-                    </div>
-                </div>
+            {productsData?.map((product, index) => {
 
+                const isLastItem = index === totalProducts - 1;
+                const bgClass = `productsMainItemBg ${isLastItem ? 'productsMainItemBgLast' : ''}`;
 
-            )}
+                return (
+                    <div className="productsMainItem" key={product.number}>
+                        {/* Use the dynamically created bgClass */}
+                        <div className={bgClass}>
+                            <Image src={product.bgImage} alt={product.name} fill className="productsMainItemImg" priority />
+                        </div>
+                        <div className="productsMainItemContent">
+                            <span className="productsMainItemNumber">{product.number}</span>
+                            <h3 className="productsMainItemName">{product.name}</h3>
+                            <Link href={product?.link ? product?.link : ""} scroll={false}>
+                                Explore More <span className="arrow"></span>
+                            </Link>
+                        </div>
+                    </div>
+                )
+            })}
         </section>
     )
 }
