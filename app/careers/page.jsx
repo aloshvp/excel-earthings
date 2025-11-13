b1057cc2d859c73e61012c4bcb4288654e6cf40b
aloshvp <aloshvp12@gmail.com>
2025-11-13 21:36:38 +0530

    careers form style

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -2,9 +2,20 @@
 import "@styles/staticbundles.scss";
 import CommonBanner from '@common/CommonBanner'
 import Image from 'next/image'
+import { useState } from "react";
 
 
 const page = () => {
+    const [fileName, setFileName] = useState("No file selected");
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setFileName(file ? file.name : "No file selected");
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();        
+    };
     return (
         <section className='careersWrap'>
             <CommonBanner pageName={"careers"} />
@@ -12,11 +23,13 @@ const page = () => {
                 <div className="container">
                     <div className="careersBody">
                         <div className="careersFormSec">
-                            <form>
-                                <h2>Looking for a Better Career Opportunity?</h2>
-                                <p>Choose a career with us, and together, let's create positive, long-lasting values and take your
-                                    career to the next level.
-                                </p>
+                            <form onSubmit={handleSubmit}>
+                                <div className="careerFormHead">
+                                    <h2>Looking for a Better Career Opportunity?</h2>
+                                    <p>Choose a career with us, and together, let's create positive, long-lasting values and take your
+                                        career to the next level.
+                                    </p>
+                                </div>
                                 <span>
                                     <input type="text" placeholder="First Name" />
                                 </span>
@@ -35,10 +48,17 @@ const page = () => {
                                 <span>
                                     <input type="text" placeholder="Your Expertise" />
                                 </span>
-                                <span>
+                                <span className="fileUpload">
                                     <i>Upload Your Resume</i>
-                                    <input type="file" />
+                                    <div>
+                                        <label htmlFor="resumeUpload" className="customFileLabel">
+                                            Choose File
+                                        </label>
+                                        <input type="file" id="resumeUpload" onChange={handleFileChange} />
+                                        <span className="fileName">{fileName}</span>
+                                    </div>
                                 </span>
+
                                 <span>
                                     <button>Submit</button>
                                 </span>
