473982426e89b8f288062af0ff59f875b76dd576
aloshvp <aloshvp12@gmail.com>
2025-11-19 21:44:43 +0530

    about-us

--- app/components/aboutUs/AboutUsInfrastructure.jsx
+++ app/components/aboutUs/AboutUsInfrastructure.jsx
@@ -1,9 +1,63 @@
+import Image from 'next/image'
 import React from 'react'
 
 const AboutUsInfrastructure = () => {
     return (
-        <section>
-
+        <section className='aboutUsInfraStructureWrap'>
+            <div className="container">
+                <div className="aboutUsInfraStructureTopCont">
+                    <div className="aboutUsInfraStructureTopContLft">
+                        <h2>Infrastructure</h2>
+                        <p>
+                            Excel Earthings has advanced infrastructural conveniences clubbed with technological
+                            expertise to offer the finest Electrical Earthing and Lightning Protection Products. Our
+                            Corporate Office, Marketing Division and Manufacturing Units are in one of the most promising
+                            cities of India – Thrissur, Kerala. Our well-designed and robust infrastructure is competent
+                            to support and manage the present and future market requirements. The stringent focus on
+                            quality and sturdiness of the entire infrastructure setup keeps us a level ahead when it
+                            comes to delivering our products in a quality rich and timely manner.
+                        </p>
+                        <p>
+                            Aimed at providing high quality business, our office and factory premises are equipped with
+                            modern manufacturing facilities, that includes machinery for manufacturing of critical components
+                            and product assembly lines, software to carry out the business analytics and market research
+                            along with well-planned logistics support. All our departments which include manufacturing,
+                            design, quality analysis, research & development, sales & marketing, accounts and logistics are
+                            managed by well experienced professionals and are provided with regular trainings for overall
+                            development. A well governed ERP System provides a harmonious collaboration between each department.
+                        </p>
+                    </div>
+                    <div className="aboutUsInfraStructureTopContRght">
+                        <em>
+                            <Image
+                                src="/images/static/infrastructure.png"
+                                alt="Infrastructure"
+                                width={845}
+                                height={845}
+                                className='img'
+                            />
+                        </em>
+                    </div>
+                </div>
+                <div className="aboutUsInfraStructureBtmCont">
+                    <h3>Infrastructure Highlights</h3>
+                    <div className='infrastructureHighlightsSec'>
+                        <ul>
+                            <li>30,000 sq.ft. well-designed work area (25000 sq.ft. Production & Warehouse; 5000 sq.ft. Marketing & Administration Office)</li>
+                            <li>Technical Design & Marketing Support</li>
+                            <li>Most Modern Machineries and Technology</li>
+                            <li>Monthly Production Capacity of 500 tons of Earth Enhancing Compounds</li>
+                            <li>Monthly Production Capacity of 100 tons of Copper Bonded Products</li>
+                        </ul>
+                        <ul>
+                            <li>30,000 sq.ft. well-designed work area (25000 sq.ft. Production & Warehouse; 5000 sq.ft. Marketing & Administration Office)</li>
+                            <li>In-House Lab Facility</li>
+                            <li>State of art conference rooms with audiovisual conference facilities</li>
+                            <li>Backup Power Supply Available</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
