96e536bc06eb5b6e4bfc8e59914d9e6e193dd5bc
aloshvp <aloshvp12@gmail.com>
2025-11-20 23:33:30 +0530

    style

--- app/components/common/Footer.jsx
+++ app/components/common/Footer.jsx
@@ -1,128 +1,139 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Footer = () => {
-    return (
-        <footer className='footerWrap'>
-            <div className="container">
-                <div className="footerBody">
 
-                    <div className="footerLogoSec">
-                        <div className="footerLogoImg">
-                            <Image src='/images/logo-excel-earthings.svg' width={340} height={100} alt="Excel Earthings" title="Excel Earthings" />
-                            <div className="dividerVertical"></div> {/* Divider between images */}
-                            <Image src='/images/logo-excel-safepower.svg' width={238} height={70} alt="Excel Safepower" title="Excel Safepower" />
+    const pathname = usePathname();
+
+    // Convert pathname → slug for className
+    const pageClass = pathname === "/"
+        ? "home"
+        : pathname.replace("/", "").toLowerCase(); // /contact-us → contact-us
+
+    return (
+        <footer className={`footerParentWrap ${pageClass}`}>
+            <div className="footerBgWrap"></div>
+            <div className="footerWrap">
+                <div className="container">
+                    <div className="footerBody">
+
+                        <div className="footerLogoSec">
+                            <div className="footerLogoImg">
+                                <Image src='/images/logo-excel-earthings.svg' width={340} height={100} alt="Excel Earthings" title="Excel Earthings" />
+                                <div className="dividerVertical"></div> {/* Divider between images */}
+                                <Image src='/images/logo-excel-safepower.svg' width={238} height={70} alt="Excel Safepower" title="Excel Safepower" />
+                            </div>
+
+                            <div className="footerContact">
+                                <div>
+                                    <b>Customer Care</b>
+                                    <span><Link href={"tel:90487 44551"}>+91 90487 44551</Link></span>
+                                </div>
+                                <div className="dividerVertical"></div> {/* Divider between contact items */}
+                                <div>
+                                    <b>Send Your Mail at</b>
+                                    <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="footerContact">
-                            <div>
-                                <b>Customer Care</b>
-                                <span><Link href={"tel:90487 44551"}>+91 90487 44551</Link></span>
+                        <div className="footerDividerHorizontal"></div>
+
+                        <div className="footerAddressSec">
+                            <div className="footerAddressSecLft">
+                                <b>Head Ofice</b>
+                                <p>7/729 (A), Mannuthy P.O. <em></em>
+                                    Thrissur, Kerala, India - 680 651 </p>
+                                <b>Email</b>
+                                <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
                             </div>
-                            <div className="dividerVertical"></div> {/* Divider between contact items */}
-                            <div>
-                                <b>Send Your Mail at</b>
+                            <div className="footerAddressSecCntr">
+                                <b>Factory</b>
+                                <p>Plot No. 26,30 & 34, SIDCO Industrial Park,<em></em>
+                                    Chelakkara, Thrissur, Kerala, India - 680 587 </p>
+                                <b>Email</b>
+                                <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
+                            </div>
+                            <div className="footerAddressSecRght">
+                                <b>Marketing Division</b>
+                                <p>1/667/5-6, Spectrum Tower, Udaya Nagar,<em></em>
+                                    Ayyanthole P.O. Thrissur, Kerala, India - 680 003 </p>
+                                <b>Email</b>
                                 <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="footerDividerHorizontal"></div>
+                        <div className="footerDividerHorizontal"></div>
 
-                    <div className="footerAddressSec">
-                        <div className="footerAddressSecLft">
-                            <b>Head Ofice</b>
-                            <p>7/729 (A), Mannuthy P.O. <em></em>
-                                Thrissur, Kerala, India - 680 651 </p>
-                            <b>Email</b>
-                            <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
-                        </div>
-                        <div className="footerAddressSecCntr">
-                            <b>Factory</b>
-                            <p>Plot No. 26,30 & 34, SIDCO Industrial Park,<em></em>
-                                Chelakkara, Thrissur, Kerala, India - 680 587 </p>
-                            <b>Email</b>
-                            <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
-                        </div>
-                        <div className="footerAddressSecRght">
-                            <b>Marketing Division</b>
-                            <p>1/667/5-6, Spectrum Tower, Udaya Nagar,<em></em>
-                                Ayyanthole P.O. Thrissur, Kerala, India - 680 003 </p>
-                            <b>Email</b>
-                            <span><Link href={"mailto:mail@excelearthings.com"}>mail@excelearthings.com</Link></span>
-                        </div>
-                    </div>
-
-                    <div className="footerDividerHorizontal"></div>
-
-                    <div className="footerAboutSec">
-                        <div className="footerAboutCont">
-                            <b>About Excel Earthings</b>
-                            <p>We are one of the Leading Manufacturer and Technical Solution
-                                <em></em>
-                                Provider in the field of Electrical Earthing and Lightning Protection. By
-                                <em></em>
-                                providing suitable designs, awareness & customized products, we
-                                <em></em>
-                                ensure Electrical Safety and Power Quality Improvements in the
-                                <em></em>
-                                Electrical Network. </p>
-                            <Link href="" scroll={false}>Learn More</Link>
-                        </div>
-                        <div className="footerLinks">
-                            <b>Quick Links</b>
-                            <div className='footerLinkNav'>
-                                <ul>
-                                    <li><Link href="/">Home</Link></li>
-                                    <li><Link href="" scroll={false}>About Us</Link></li>
-                                    <li><Link href="/products">Products</Link></li>
-                                    <li><Link href="/certifications-and-quality-assurance" >Certifications & Quality Assurance</Link></li>
-                                    <li><Link href="/approvals-and-associates">Approvals & Associates</Link></li>
-                                    <li><Link href="/applications">Applications</Link></li>
-                                    <li><Link href="/dealership" >Dealership</Link></li>
-                                </ul>
-                                <ul>
-                                    <li><Link href="/e-learning">E-Learning</Link></li>
-                                    <li><Link href="/transformer-earthing-design">Transformer Earthing Design</Link></li>
-                                    <li><Link href="/faq" >Faq</Link></li>
-                                    <li><Link href="/careers">Careers</Link></li>
-                                    <li><Link href="/insights-and-updates">Insights & Updates</Link></li>
-                                    <li><Link href="/contact-us">Contact Us</Link></li>
-                                </ul>
+                        <div className="footerAboutSec">
+                            <div className="footerAboutCont">
+                                <b>About Excel Earthings</b>
+                                <p>We are one of the Leading Manufacturer and Technical Solution
+                                    <em></em>
+                                    Provider in the field of Electrical Earthing and Lightning Protection. By
+                                    <em></em>
+                                    providing suitable designs, awareness & customized products, we
+                                    <em></em>
+                                    ensure Electrical Safety and Power Quality Improvements in the
+                                    <em></em>
+                                    Electrical Network. </p>
+                                <Link href="" scroll={false}>Learn More</Link>
                             </div>
-                        </div>
-                        <div className="footerSocialMedia">
-                            <b>Follow Us</b>
-                            <div className="footerSocialMediaLinks">
-                                <Link href="" scroll={false}>
-                                    <Image
-                                        src='/images/icons/facebook.svg'
-                                        alt="facebook"
-                                        title="facebook"
-                                        width={10}
-                                        height={10}
-                                    />
-                                </Link>
-                                <Link href="" scroll={false}>
-                                    <Image
-                                        src='/images/icons/instagram.svg'
-                                        alt="instagram"
-                                        title="instagram"
-                                        width={20}
-                                        height={20}
-                                    />
-                                </Link>
-                                <Link href="" scroll={false}>
-                                    <Image
-                                        src='/images/icons/youtube.svg'
-                                        alt="youtube"
-                                        title="youtube"
-                                        width={20}
-                                        height={20}
-                                    />
-                                </Link>
-                                {/* <Link href="" scroll={false}>
+                            <div className="footerLinks">
+                                <b>Quick Links</b>
+                                <div className='footerLinkNav'>
+                                    <ul>
+                                        <li><Link href="/">Home</Link></li>
+                                        <li><Link href="" scroll={false}>About Us</Link></li>
+                                        <li><Link href="/products">Products</Link></li>
+                                        <li><Link href="/certifications-and-quality-assurance" >Certifications & Quality Assurance</Link></li>
+                                        <li><Link href="/approvals-and-associates">Approvals & Associates</Link></li>
+                                        <li><Link href="/applications">Applications</Link></li>
+                                        <li><Link href="/dealership" >Dealership</Link></li>
+                                    </ul>
+                                    <ul>
+                                        <li><Link href="/e-learning">E-Learning</Link></li>
+                                        <li><Link href="/transformer-earthing-design">Transformer Earthing Design</Link></li>
+                                        <li><Link href="/faq" >Faq</Link></li>
+                                        <li><Link href="/careers">Careers</Link></li>
+                                        <li><Link href="/insights-and-updates">Insights & Updates</Link></li>
+                                        <li><Link href="/contact-us">Contact Us</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="footerSocialMedia">
+                                <b>Follow Us</b>
+                                <div className="footerSocialMediaLinks">
+                                    <Link href="" scroll={false}>
+                                        <Image
+                                            src='/images/icons/facebook.svg'
+                                            alt="facebook"
+                                            title="facebook"
+                                            width={10}
+                                            height={10}
+                                        />
+                                    </Link>
+                                    <Link href="" scroll={false}>
+                                        <Image
+                                            src='/images/icons/instagram.svg'
+                                            alt="instagram"
+                                            title="instagram"
+                                            width={20}
+                                            height={20}
+                                        />
+                                    </Link>
+                                    <Link href="" scroll={false}>
+                                        <Image
+                                            src='/images/icons/youtube.svg'
+                                            alt="youtube"
+                                            title="youtube"
+                                            width={20}
+                                            height={20}
+                                        />
+                                    </Link>
+                                    {/* <Link href="" scroll={false}>
                                     <Image
                                         src='/images/icons/linkedin.svg'
                                         alt="linkedin"
@@ -131,30 +142,31 @@ const Footer = () => {
                                         height={15}
                                     />
                                 </Link> */}
-                                <Link href="" scroll={false}>
-                                    <Image
-                                        src='/images/icons/twitter.svg'
-                                        alt="twitter"
-                                        title="twitter"
-                                        width={15}
-                                        height={15}
-                                    />
-                                </Link>
+                                    <Link href="" scroll={false}>
+                                        <Image
+                                            src='/images/icons/twitter.svg'
+                                            alt="twitter"
+                                            title="twitter"
+                                            width={15}
+                                            height={15}
+                                        />
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="footerDividerHorizontal"></div>
+                        <div className="footerDividerHorizontal"></div>
 
-                    <div className="footerBottomSec">
-                        <div className="footerBottomSecLft">
-                            <p>© {new Date().getFullYear()} M/S.Excel Earthings. All Rights Reserved.</p>
+                        <div className="footerBottomSec">
+                            <div className="footerBottomSecLft">
+                                <p>© {new Date().getFullYear()} M/S.Excel Earthings. All Rights Reserved.</p>
+                            </div>
+                            <div className="footerBottomSecRght">
+                                <em>Developed by Marin Infotech</em>
+                            </div>
                         </div>
-                        <div className="footerBottomSecRght">
-                            <em>Developed by Marin Infotech</em>
-                        </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </footer >
