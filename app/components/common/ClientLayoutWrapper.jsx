fa6de31d5474258c290f00295a2da660136f47fe
aloshvp <aloshvp12@gmail.com>
2025-11-20 20:16:49 +0530

    about-us

--- app/components/common/ClientLayoutWrapper.jsx
+++ app/components/common/ClientLayoutWrapper.jsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import dynamic from "next/dynamic";
+import Lenis from "lenis";
 import Header from "./Header";
 import { dynamicScrollPages } from "@utils/CommonData";
+
 const Footer = dynamic(() => import("./Footer"), {
     ssr: false,
 });
@@ -12,6 +15,21 @@ export default function ClientLayoutWrapper({ children }) {
 
     const pathname = usePathname();
     const isAdmin = pathname.startsWith("/admin");
+
+    useEffect(() => {
+        const lenis = new Lenis();
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
 
     // Only show header/footer on non-admin, non-login pages
     const showLayout = !isAdmin;
