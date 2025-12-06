38842db17a68510deebe725f9bf169c16131f94d
aloshvp <aloshvp12@gmail.com>
2025-12-06 08:11:40 +0530

    Update page.jsx

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -2,9 +2,31 @@
 import "@styles/staticbundles.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
+import AOS from 'aos';
 import { productsData } from "@utils/homeData";
 
 const page = () => {
+    useEffect(() => {
+        // Ensure AOS is refreshed when this page loads
+        const refreshAOS = () => {
+            if (typeof AOS !== 'undefined') {
+                AOS.refresh();
+            }
+        };
+
+        // Refresh immediately and after delays to ensure animations work
+        refreshAOS();
+        const timer1 = setTimeout(refreshAOS, 100);
+        const timer2 = setTimeout(refreshAOS, 300);
+        const timer3 = setTimeout(refreshAOS, 600);
+
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+            clearTimeout(timer3);
+        };
+    }, []);
 
     const totalProducts = productsData?.length;
 
@@ -22,9 +44,9 @@ const page = () => {
                             <Image src={product.bgImage} alt={product.name} fill className="productsMainItemImg" priority />
                         </div>
                         <div className="productsMainItemContent">
-                            <span className="productsMainItemNumber" data-aos="zoom-in" data-aos-delay={index * 50} data-aos-duration="1200" data-aos-easing="ease-out-cubic" data-aos-offset="510" data-aos-once="false">{product.number}</span>
-                            <h3 className="productsMainItemName" data-aos="fade-up" data-aos-delay={index * 100 + 200} data-aos-duration="800" data-aos-easing="ease-out-cubic" data-aos-offset="150">{product.name}</h3>
-                            <Link href={product?.link ? product?.link : ""} data-aos="fade-up" data-aos-delay={index * 100 + 300} data-aos-duration="800" data-aos-easing="ease-out-cubic" data-aos-offset="150">
+                            <span className="productsMainItemNumber" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-out-quart" data-aos-offset="150" data-aos-once="false">{product.number}</span>
+                            <h3 className="productsMainItemName" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-out-quart" data-aos-offset="150">{product.name}</h3>
+                            <Link href={product?.link ? product?.link : ""} data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-out-quart" data-aos-offset="150">
                                 Explore More <span className="arrow"></span>
                             </Link>
                         </div>
